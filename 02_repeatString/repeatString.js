92c37fd30fdedd948af10768d07faea973e5882c
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-05 00:58:39 +0100

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, repeatCount) {
+  if (repeatCount < 0) return "ERROR";
+  let repeatedString = "";
 
+  for (let i = 0; i < repeatCount; i++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
