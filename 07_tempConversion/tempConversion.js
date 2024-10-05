65996cef9ed884707106b2deff9a5ae35dae0375
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-05 02:20:35 +0100

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const convertedTemperature = 5 / 9 * (temperature - 32);
+  
+  return Math.round(convertedTemperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const convertedTemperature = 9 / 5 * temperature + 32;
+  
+  return Math.round(convertedTemperature * 10) / 10;
 };
 
 // Do not edit below this line
