82af3e777b77c9ff27cac11e5d4501ede173296a
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-05 01:43:33 +0100

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!isAPositiveInteger(start) || !isAPositiveInteger(end)) {
+    return 'ERROR';
+  } 
 
+  let sum = 0;
+  let upperBoundary = Math.max(start, end);
+  let lowerBoundary = Math.min(start, end);
+
+  for (let i = lowerBoundary; i <= upperBoundary; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+const isAPositiveInteger = (number) => {
+  return number > 0 && Math.floor(number) === number;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
