fba2e27f41267872b157f86966417afc5c821855
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-14 03:40:36 +0100

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  const flatenedPhrase = phrase.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return flatenedPhrase === flatenedPhrase.split('').reverse().join('')
 };
 
 // Do not edit below this line
