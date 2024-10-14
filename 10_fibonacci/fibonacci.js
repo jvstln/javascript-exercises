42447125266a5c87fdfe9982e7d5a3d390b85f28
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-14 03:53:59 +0100

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
+  if (term < 0) return 'OOPS'
+  if (term == 0) return 0;
 
+  let prev1 = 0;
+  let prev2 = 1;
+
+  for (let i = 2; i <= term; i++) {
+    let currentFib = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = currentFib;
+  }
+  return prev2
 };
 
 // Do not edit below this line
