4bade8c19ae6977f033e16b1c2dade9903885706
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-14 04:10:54 +0100

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce((oldest, person) => {
+    const age1 = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const age2 = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    return age1 > age2 ? oldest : person;
+  })
 };
 
 // Do not edit below this line
